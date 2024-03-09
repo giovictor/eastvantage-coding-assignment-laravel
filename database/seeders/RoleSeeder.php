638c5f0c464d33645fc2ca875c46928b72c6ce6c
Gio Victor Rodriguez <giorodriguez021@gmail.com>
2024-03-10 00:53:29 +0800

    Created seeder for users and roles

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $roles = [
+            'Author',
+            'Editor',
+            'Subscriber',
+            'Administrator',
+        ];
+
+        foreach($roles AS $role) {
+            Role::create([
+                'role' => $role
+            ]);
+        }
+    }
+}
